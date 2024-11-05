ba901d80cacce5eace47d9ecd52ecb98e27300f4
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2024-11-06 01:52:31 +0200

    Minor changes in CLI

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -6,38 +6,33 @@ public class App {
     public static final Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Welcome to tic-tac-toe!\n");
+        System.out.println("\033[1mWhich mode you want?\033[0m");
         System.out.println("1. Player vs Player.");
-        System.out.println("2. Player vs Computer.");
-        int mode;
-        do {
-            System.out.print("Which mode you want? ");
-            mode = input.nextInt();
-            if (mode == 0) {
-                return;
-            } else if (mode == 1) {
-                Game game = new Game();
-                Player x = new Player(game, 1), o = new Player(game, 0);
-                do {
+        System.out.print("2. Player vs Computer.\n\n~> ");
+        int mode = input.nextInt();
+
+        if (mode == 1) {
+            Game game = new Game();
+            Player x = new Player(game, 1), o = new Player(game, 0);
+            do {
+                game.printBoard();
+                System.out.println((game.xTurn() ? 'X' : 'O') + "'s turn.");
+                (game.xTurn() ? x : o).play();
+                Game.State state = game.state();
+                if (state == Game.State.TIE) {
                     game.printBoard();
-                    System.out.println((game.xTurn() ? 'X' : 'O') + "'s turn.");
-                    (game.xTurn() ? x : o).play();
-                    Game.State state = game.state();
-                    if (state == Game.State.TIE)  {
-                        game.printBoard();
-                        System.out.println("Tie!");
-                        return;
-                    } else if (state == Game.State.WIN) {
-                        game.printBoard();
-                        System.out.println((game.xTurn() ? 'O' : 'X') + " wins!");
-                        return;
-                    }
-                } while (true);
-            } else if (mode == 2) {
-                System.out.println("Not implemented yet :)");
-            } else {
-                System.out.println("Invalid mode!");
-            }
-        } while (true);
+                    System.out.println("Tie!");
+                    return;
+                } else if (state == Game.State.WIN) {
+                    game.printBoard();
+                    System.out.println((game.xTurn() ? 'O' : 'X') + " wins!");
+                    return;
+                }
+            } while (true);
+        } else if (mode == 2) {
+            System.out.println("Not implemented yet :)");
+        } else if (mode != 0) {
+            System.out.println("Invalid mode!");
+        }
     }
 }
